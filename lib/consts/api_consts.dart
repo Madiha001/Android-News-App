cbf47e40c08595590da96490dd7affdb72697289
Madiha <madiha.bcss19@iba-suk.edu.pk>
2023-04-09 17:08:00 +0500

    Android News App

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,5 @@
+const String baseUrl = "newsapi.org";
+const String apiKey = "84a442d2ffca4e85832a4b68696ee890"; //84a442d2ffca4e85832a4b68696ee890
+
+// Be careful use your link
+const String baseUrlFireBase = "newsteaching-7edbe-default-rtdb.firebaseio.com";
